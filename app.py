4706428a82bcbc1f31566e29a0ec81e26bb763e4
josehildemaro <hildemaro.pe@gmail.com>
2024-10-24 15:15:43 -0500

    Configure GitHub Actions for Azure deployment

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "¡Hola, Azure!"
+    return "¡Hola, Azure 2024!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
